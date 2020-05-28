1661e08dd58892a05a0e32467ef3f3e143065428
mcrwayfun <mcrwayfun@gmail.com>
2020-05-28 10:02:35 +0800

    fileds

---
+++ gopkg/strings/fileds/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main(){
+	fmt.Printf("Fields are: %q", strings.Fields("  foo bar  baz   "))
+	// output
+	// fmt.Printf("Fields are: %q", strings.Fields("  foo bar  baz   "))
+}
